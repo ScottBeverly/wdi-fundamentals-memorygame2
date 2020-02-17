fee951486e69d40a1987d73164756c2f8bdc4a5c
scottie19 <60015404+scottie19@users.noreply.github.com>
2020-02-17 18:56:43 -0500

    Add files via upload

--- Mgame/js/main.js
+++ Mgame/js/main.js
@@ -1,7 +1,15 @@
-console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardfour = "king";
-console.log("user flipped" + " " + cardOne);
-console.log("user flipped" + " " + cardThree);
+const cards =["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+const cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("user flipped" + cardOne);
+const cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("you found a match!");
+}
+else if (cardsInPlay[0] !== cardsInPlay[1]) {
+	alert(" sorry, try again");
+}
+}
