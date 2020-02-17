f1b8201358ec3d887bd23967fd25afb6c6cdf3c6
scottie19 <60015404+scottie19@users.noreply.github.com>
2020-02-17 18:17:51 -0500

    Add files via upload

---
+++ Mgame/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardfour = "king";
+console.log("user flipped" + " " + cardOne);
+console.log("user flipped" + " " + cardThree);
