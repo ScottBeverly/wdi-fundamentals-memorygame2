d9c8330195d8767983bfc59c67f5622bf323858f
scottie19 <60015404+scottie19@users.noreply.github.com>
2020-02-17 19:52:41 -0500

    Add files via upload

--- Mgame/js/main.js
+++ Mgame/js/main.js
@@ -1,4 +1,28 @@
-const cards =["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank:'queen',
+	suit:'diamonds',
+	cardImage:	"images/queen-of-diamonds.png",
+},
+{
+	rank:'king',
+	suit:'hearts',
+	cardImage:"images/king-of-hearts.png",
+
+},
+{
+	rank:'king',
+	suit:'diamonds',
+	cardImage:"images/king-of-diamonds.png",
+
+}
+];
+
 const cardsInPlay = [];
 
 function  checkformatch() {
@@ -10,16 +34,16 @@ function  checkformatch() {
 }
 
 function flipCard(CardId) {
-	const cardTwo = cards[2];
-	const cardOne = cards[0];
-	cardsInPlay.push(cardOne);
-	cardsInPlay.push(cardTwo);
-	console.log("user flipped" + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("user flipped" + " " + cards[cardId].rank);
+	console.log("user flipped" + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
+
+if (cardsInPlay.length === 2){
+
+}
 }
 flipCard(0);
 flipCard(2);
-if (cardsInPlay.length === 2){
-
-checkformatch();
-}
